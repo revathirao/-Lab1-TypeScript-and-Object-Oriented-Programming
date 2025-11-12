1738de340adaa3d42285d7de9fd1066a2da4409a
Revathi <revathi9605@gmail.com>
2025-11-12 17:31:40 -0500

    created utility

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 
-
+import Products from "../src/models/Product
 import  PhysicalProduct from "../src/models/PhysicalProduct"
 import DigitalProduct from "../src/models/DigitalProduct"
 import { calculateTax } from "./utils/taxCalculator.js";
+
+PhysicalProduct = new Physicalproduct
+
+
+// Create an array of mixed products
+
